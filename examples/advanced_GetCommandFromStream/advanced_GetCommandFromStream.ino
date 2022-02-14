1f536ee290e42daf2d9e85b328b8345dd57d1635
Douglas Quigg <dquigg123@gmail.com>
2022-02-14 14:04:26 -0500

    Update advanced_GetCommandFromStream.ino

--- examples/advanced_GetCommandFromStream/advanced_GetCommandFromStream.ino
+++ examples/advanced_GetCommandFromStream/advanced_GetCommandFromStream.ino
@@ -2,12 +2,17 @@
 
 const uint16_t output_buffer_size = 128;
 char output_buffer[output_buffer_size] = {'\0'};
-uint16_t string_pos = 0; 
+uint16_t string_pos = 0;
 UserInput inputHandler(output_buffer, &string_pos, 128);
 
 void uc_unrecognized(UserInput* inputProcess)
 {
-  Serial.println(F("made it to uc_unrecognized"));  
+  Serial.println(F("made it to uc_unrecognized"));
+}
+
+void uc_settings(UserInput* inputProcess)
+{
+  //inputHandler->ListUserInputSettings(inputHandler);
 }
 
 void uc_help(UserInput* inputProcess)
@@ -15,24 +20,31 @@ void uc_help(UserInput* inputProcess)
   inputProcess->ListUserCommands();
 }
 
-UserCallbackFunctionParameters uc_help_("help", uc_help);
+PGM_P const PROGMEM CMD_HELP = "help";
+PGM_P const PROGMEM CMD_INPUT_SETTINGS = "inputSettings";
 
+UserCallbackFunctionParameters uc_help_(CMD_HELP, uc_help, 0, 0);
+UserCallbackFunctionParameters uc_settings_(CMD_INPUT_SETTINGS, uc_settings, 0, 0);
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(115200); //  set up Serial0
 
-  inputHandler.SetDefaultHandler(uc_unrecognized);
-  inputHandler.AddUserCommand(&uc_help_);
+  inputHandler.SetDefaultHandler(uc_unrecognized);  // set default function, called when user input has no match or is not valid
+  inputHandler.AddUserCommand(&uc_help_); // lists commands available to the user
+  inputHandler.AddUserCommand(&uc_settings_); // lists UserInput class settings
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   inputHandler.GetCommandFromStream(Serial);
-  Serial.println(output_buffer);
-  string_pos = 0;
-  for(uint16_t i = 0; i < output_buffer_size; ++i)
+  if (inputHandler.OutputIsAvailable() == true)
   {
-    output_buffer[i] = '\0';
+    Serial.println(output_buffer);
+    string_pos = 0;
+    for (uint16_t i = 0; i < output_buffer_size; ++i)
+    {
+      output_buffer[i] = '\0';
+    }
   }
 }
