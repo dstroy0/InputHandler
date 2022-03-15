b269d782f626d07d904e8a7f4a32228a43ee484b
Douglas Quigg <dquigg123@gmail.com>
2022-03-15 16:27:38 -0400

    docs, startup delay

--- examples/arduino/basic/GetCommandFromStream/GetCommandFromStream.ino
+++ examples/arduino/basic/GetCommandFromStream/GetCommandFromStream.ino
@@ -36,14 +36,14 @@ void uc_hello_(UserInput* inputProcess)
 */
 const Parameters type_test_param[1] PROGMEM =
 {
-  uc_hello_,           // function pointer
-  "hello",             // command string
-  5,                   // string length
-  0,                   // command depth
-  0,                   // subcommands
-  argument_type_array, // argument handling
-  0,                   // minimum expected number of arguments
-  0,                   // maximum expected number of arguments
+  uc_hello_,    // function name
+  "hello",      // command string
+  5,            // string length
+  0,            // command depth
+  0,            // subcommands
+  no_arguments, // argument handling
+  0,            // minimum expected number of arguments
+  0,            // maximum expected number of arguments
   /*
     UITYPE arguments
   */
@@ -55,6 +55,7 @@ CommandConstructor uc_test_(type_test_param);
 
 void setup()
 {
+  delay(500); // startup delay for reprogramming (prevents restart)
   Serial.begin(115200); //  set up Serial
   while (!Serial); //  wait for user
 
