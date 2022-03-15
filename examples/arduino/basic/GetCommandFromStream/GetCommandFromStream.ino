255a468a174cc010b24d3044dc7bf82e5773a6ab
Douglas Quigg <dquigg123@gmail.com>
2022-03-15 16:18:40 -0400

    simplify basic example

--- examples/arduino/basic/GetCommandFromStream/GetCommandFromStream.ino
+++ examples/arduino/basic/GetCommandFromStream/GetCommandFromStream.ino
@@ -1,7 +1,7 @@
 /**
-   @file basicGetCommandFromStream.ino
+   @file GetCommandFromStream.ino
    @author Douglas Quigg (dstroy0 dquigg123@gmail.com)
-   @brief A basic example
+   @brief A basic UserInput example
    @version 0.1
    @date 2022-02-22
 
@@ -10,6 +10,9 @@
 
 #include <InputHandler.h>
 
+/*
+  UserInput constructor
+*/
 UserInput inputHandler;
 
 /*
@@ -21,79 +24,31 @@ void uc_unrecognized(UserInput* inputProcess)
 }
 
 /*
-   test all available input types
+   test callback function
 */
-void uc_test_input_types(UserInput* inputProcess)
+void uc_hello_(UserInput* inputProcess)
 {
-  char* str_ptr = inputProcess->NextArgument();                                     //  init str_ptr and point it at the next argument input by the user
-  char* strtoul_ptr = 0;                                                            //  this is for strtoul
-  uint32_t strtoul_result = strtoul(str_ptr, &strtoul_ptr, 10);                     // get the result in base10
-  uint8_t eight_bit = (strtoul_result <= UINT8_MAX) ? (uint8_t)strtoul_result : 0U; // if the result is less than UINT8_MAX then set eight_bit, else eight_bit = 0
-
-  str_ptr = inputProcess->NextArgument();
-  strtoul_ptr = 0;
-  strtoul_result = strtoul(str_ptr, &strtoul_ptr, 10);
-  uint16_t sixteen_bit = (strtoul_result <= UINT16_MAX) ? (uint16_t)strtoul_result : 0U;
-
-  str_ptr = inputProcess->NextArgument();
-  strtoul_ptr = 0;
-  uint32_t thirtytwo_bit = strtoul(str_ptr, &strtoul_ptr, 10);
-
-  str_ptr = inputProcess->NextArgument();
-  int sixteen_bit_int = atoi(str_ptr);
-
-  str_ptr = inputProcess->NextArgument();
-  float thirtytwo_bit_float = (float)atof(str_ptr);
-
-  str_ptr = inputProcess->NextArgument();
-  char _char = *str_ptr;
-
-  str_ptr = inputProcess->NextArgument();
-  char c_string[64] = {'\0'};
-  snprintf_P(c_string, 64, PSTR("%s"), str_ptr);
-
-  char float_buffer[32] = {'\0'}; //  dtostrf buffer
-  char out[164] = {'\0'};         //  function output buffer
-  uint16_t string_pos = 0;        // function output buffer index
-
-  /*
-       format out[] with all of the arguments received
-  */
-  string_pos += snprintf_P(out + string_pos, 164,
-                           PSTR("Test user input types:\n"
-                                "uint8_t %u\nuint16_t %u\nuint32_t %lu\nint %d\nfloat %s\nchar %c\nc-string %s\n"),
-                           eight_bit,
-                           sixteen_bit,
-                           thirtytwo_bit,
-                           sixteen_bit_int,
-                           dtostrf(thirtytwo_bit_float, 2, 3, float_buffer),
-                           _char,
-                           c_string);
-  Serial.print(out);
+  Serial.println(F("hello"));
 }
 
+/*
+  command parameters
+*/
 const Parameters type_test_param[1] PROGMEM =
 {
-  uc_test_input_types, // function pointer
-  "test",              // command string
-  4,                   // string length
+  uc_hello_,           // function pointer
+  "hello",             // command string
+  5,                   // string length
   0,                   // command depth
   0,                   // subcommands
   argument_type_array, // argument handling
-  8,                   // minimum expected number of arguments
-  8,                   // maximum expected number of arguments
+  0,                   // minimum expected number of arguments
+  0,                   // maximum expected number of arguments
   /*
     UITYPE arguments
   */
   {
-    UITYPE::UINT8_T,    // 8-bit  uint
-    UITYPE::UINT16_T,   // 16-bit uint
-    UITYPE::UINT32_T,   // 32-bit uint
-    UITYPE::INT16_T,    // 16-bit int
-    UITYPE::FLOAT,      // 32-bit float
-    UITYPE::CHAR,       // char
-    UITYPE::C_STRING,   // c-string, pass without quotes if there are no spaces, or pass with quotes if there are
-    UITYPE::NOTYPE      // special type, no type validation performed
+    UITYPE::NO_ARGS
   }
 };
 CommandConstructor uc_test_(type_test_param);
@@ -106,7 +61,7 @@ void setup()
   inputHandler.DefaultFunction(uc_unrecognized); // set default function, called when user input has no match or is not valid
   inputHandler.AddCommand(uc_test_);             // input type test
 
-  Serial.println(F("enter test 1 2 3 4 5 a \"bb\" ccc to test user input types."));
+  Serial.println(F("enter <hello> to test."));
 }
 
 void loop()
