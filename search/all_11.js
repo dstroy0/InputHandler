c0a1d78c0c00b42cf25ddb69dd3e1136c8f2bd4f
Douglas Quigg <dquigg123@gmail.com>
2022-03-16 20:33:20 -0400

    update

--- search/all_11.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['validateuserinput_0',['validateUserInput',['../classUserInput.html#a53407496c37967a3186bd136f5620dd3',1,'UserInput']]]
-];
--- search/enumvalues_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['t_5fdelim_5fe_0',['t_delim_e',['../group__UserInput.html#gga85e7eeb0bdd4436bf4ac3ff5e179c73aa19085ce12bc0507d9972232f4863baea',1,'InputHandler.h']]]
-];
