833da5fd17c2179eb2faf226184f56a2393b48cf
Douglas Quigg <dquigg123@gmail.com>
2022-03-21 14:05:03 -0400

    docs

--- src/InputHandler.h
+++ src/InputHandler.h
@@ -232,7 +232,7 @@ public:
     void addCommand(CommandConstructor &command);
 
     /**
-     * @brief allocates memory for _data_pointers_, sets _begin_
+     * @brief allocates memory for `_data_pointers_`, sets `_begin_`
      *
      * @return true if allocation successful
      * @return false if allocation unsuccessful
@@ -240,14 +240,14 @@ public:
     bool begin();
 
     /**
-     * @brief lists commands that will respond to user input if _begin_ == true
+     * @brief lists commands that will respond to user input if `_begin_` == true
      * else it will inform the user to use begin() in setup()
      */
     void listCommands();
 
     /**
      * @brief read command(s) from a uint8_t (unsigned char) buffer
-     * silent return if _begin_ == false
+     * silent return if `_begin_` == false
      * @param data a buffer with characters
      * @param len the size of the buffer
      */
@@ -258,7 +258,7 @@ public:
      *
      * https://www.arduino.cc/reference/en/language/functions/communication/stream/
      *
-     * silent return if _begin_ == false
+     * silent return if `_begin_` == false
      * @param stream the stream to reference
      * @param rx_buffer_size the size of our receive buffer
      */
