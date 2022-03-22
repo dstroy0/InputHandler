33deb65f55ad62d8c7066213143155dd17b25569
Douglas Quigg <dquigg123@gmail.com>
2022-03-22 14:41:57 -0400

    update format

--- examples/arduino/advanced/NestedCommands/NestedCommands.ino
+++ examples/arduino/advanced/NestedCommands/NestedCommands.ino
@@ -21,11 +21,11 @@ char output_buffer[512] = {'\0'}; //  output buffer
   UserInput constructor
 */
 UserInput inputHandler(/* UserInput's output buffer */ output_buffer,
-                       /* size of UserInput's output buffer */ 512,
-                       /* username */ "",
-                       /* end of line characters */ "\r\n",
-                       /* token delimiter */ " ",
-                       /* c-string delimiter */ "\"");
+    /* size of UserInput's output buffer */ 512,
+    /* username */ "",
+    /* end of line characters */ "\r\n",
+    /* token delimiter */ " ",
+    /* c-string delimiter */ "\"");
 
 /*
    default function, called if nothing matches or if there is an error
@@ -50,58 +50,62 @@ void uc_nest_two(UserInput *inputProcess)
 }
 
 const PROGMEM Parameters nested_prms[3] =
+{
+  { // root command
+    uc_unrecognized,          // this is allowed to be NULL, if this is NULL and the terminating subcommand function ptr is also NULL nothing will launch
+    "launch",                 // command string
+    6,                        // command string characters
+    root,                     // parent id
+    root,                     // this command id
+    root,                     // command depth
+    2,                        // subcommands
+    UI_ARG_HANDLING::no_args, // argument handling
+    0,                        // minimum expected number of arguments
+    0,                        // maximum expected number of arguments
+    /*
+      UITYPE arguments
+    */
     {
-        {                          // root command
-         uc_unrecognized,          // this is allowed to be NULL, if this is NULL and the terminating subcommand function ptr is also NULL nothing will launch
-         "launch",                 // command string
-         6,                        // command string characters
-         root,                     // parent id
-         root,                     // this command id
-         root,                     // command depth
-         2,                        // subcommands
-         UI_ARG_HANDLING::no_args, // argument handling
-         0,                        // minimum expected number of arguments
-         0,                        // maximum expected number of arguments
-         /*
-           UITYPE arguments
-         */
-         {
-             UITYPE::NO_ARGS // use NO_ARGS if the function expects no arguments
-         }},
-        {                          // subcommand depth one
-         uc_nest_one,              // unique function
-         "one",                    // command string
-         3,                        // command string characters
-         root,                     // parent id
-         1,                        // this command id
-         1,                        // command depth
-         0,                        // subcommands
-         UI_ARG_HANDLING::no_args, // argument handling
-         0,                        // minimum expected number of arguments
-         0,                        // maximum expected number of arguments
-         /*
-           UITYPE arguments
-         */
-         {
-             UITYPE::NO_ARGS // use NO_ARGS if the function expects no arguments
-         }},
-        {                          // subcommand depth one
-         uc_nest_two,              // unique function
-         "two",                    // command string
-         3,                        // command string characters
-         root,                     // parent id
-         2,                        // this command id
-         1,                        // command depth
-         0,                        // subcommands
-         UI_ARG_HANDLING::no_args, // argument handling
-         0,                        // minimum expected number of arguments
-         0,                        // maximum expected number of arguments
-         /*
-           UITYPE arguments
-         */
-         {
-             UITYPE::NO_ARGS // use NO_ARGS if the function expects no arguments
-         }}};
+      UITYPE::NO_ARGS // use NO_ARGS if the function expects no arguments
+    }
+  },
+  { // subcommand depth one
+    uc_nest_one,              // unique function
+    "one",                    // command string
+    3,                        // command string characters
+    root,                     // parent id
+    1,                        // this command id
+    1,                        // command depth
+    0,                        // subcommands
+    UI_ARG_HANDLING::no_args, // argument handling
+    0,                        // minimum expected number of arguments
+    0,                        // maximum expected number of arguments
+    /*
+      UITYPE arguments
+    */
+    {
+      UITYPE::NO_ARGS // use NO_ARGS if the function expects no arguments
+    }
+  },
+  { // subcommand depth one
+    uc_nest_two,              // unique function
+    "two",                    // command string
+    3,                        // command string characters
+    root,                     // parent id
+    2,                        // this command id
+    1,                        // command depth
+    0,                        // subcommands
+    UI_ARG_HANDLING::no_args, // argument handling
+    0,                        // minimum expected number of arguments
+    0,                        // maximum expected number of arguments
+    /*
+      UITYPE arguments
+    */
+    {
+      UITYPE::NO_ARGS // use NO_ARGS if the function expects no arguments
+    }
+  }
+};
 CommandConstructor uc_nested_example_(nested_prms, _N_prms(nested_prms), 2); //  uc_help_ has a command string, and function specified
 
 void setup()
