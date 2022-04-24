72e170348555d36e7fa0c8d4cc79488e83e7397c
Doug <dquigg123@gmail.com>
2022-04-24 19:59:54 -0400

    Delete docs/html directory

--- docs/html/d5/d8e/free_ram_8h.js
+++
@@ -1,4 +0,0 @@
-var free_ram_8h =
-[
-    [ "freeRam", "d5/d8e/free_ram_8h.html#aac7b29dc45caaaca67299571f6a2dcc0", null ]
-];
--- docs/html/search/files_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['freeram_2eh_0',['freeRam.h',['../d5/d8e/free_ram_8h.html',1,'']]],
-  ['functions_2eh_1',['functions.h',['../d8/d5c/functions_8h.html',1,'']]]
-];
