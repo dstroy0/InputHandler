53c1befe87054c4813ef7c7a775982a36a38bfb7
Doug <dquigg123@gmail.com>
2022-04-24 19:55:32 -0400

    Delete html directory

--- html/dir_2417a6421416a4a5ac02e9b167a7bacc.js
+++
@@ -1,5 +0,0 @@
-var dir_2417a6421416a4a5ac02e9b167a7bacc =
-[
-    [ "CLI", "dir_d6b94e127025bf2cfdded53c78950f65.html", "dir_d6b94e127025bf2cfdded53c78950f65" ],
-    [ "MultiFile.ino", "d6/d12/_multi_file_8ino.html", "d6/d12/_multi_file_8ino" ]
-];
--- html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['addcommand_0',['addCommand',['../dc/d4b/class_user_input.html#aa6067a65dc10a5c4546ce789a3edd4d1',1,'UserInput']]]
-];
