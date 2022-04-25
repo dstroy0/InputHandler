9ab85c04d11ebbfc2437b789cd6b4d9904332a2e
Doug <dquigg123@gmail.com>
2022-04-25 12:10:24 -0400

    Delete html directory

--- html/search/enumvalues_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['char_0',['CHAR',['../de/d8a/group___user_input.html#gga70e7c464dbd2c5c26fa63684d9dfdd70a027acd3aa6b1dd7f26119d3cf0f9a063',1,'InputHandler.h']]]
-];
--- html/search/enumvalues_9.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['start_5fstop_0',['START_STOP',['../de/d8a/group___user_input.html#gga70e7c464dbd2c5c26fa63684d9dfdd70af749dae7024a2aceb2b15cb807300aef',1,'InputHandler.h']]]
-];
