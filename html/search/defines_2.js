e1b871a21153c44d634de50f08613a8d4eb7c4a0
dstroy0 <dstroy0@users.noreply.github.com>
2022-04-26 13:55:59 +0000

    deploy: 7e6ab0e163cad6f571c2fa1eaa9d738b51c3b44a

---
+++ html/search/defines_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['factoryreset_5fenable_0',['FACTORYRESET_ENABLE',['../db/d8e/_b_l_e___u_a_r_t_8ino.html#af2129e4fb10289982606b136a1d9f1a8',1,'BLE_UART.ino']]]
+];
---
+++ html/search/functions_e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['parsesentence_0',['parseSentence',['../df/d2a/class_n_m_e_aparse.html#a62f766f5171a6651a211442ca2529e76',1,'NMEAparse::parseSentence(uint8_t *buffer, size_t len)'],['../df/d2a/class_n_m_e_aparse.html#af0551afb659ca88b6c98fd111672507e',1,'NMEAparse::parseSentence(Stream &amp;stream)']]]
+];
