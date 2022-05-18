e9ec086817040a5aa6496d28ddd2a50156121f01
Douglas Quigg <dquigg123@gmail.com>
2022-05-18 19:23:17 -0400

    Update freeRam.h
    docs

--- src/utility/freeRam.h
+++ src/utility/freeRam.h
@@ -1,9 +1,27 @@
-#if !defined(__FREERAM_H__)
-    #define __FREERAM_H__
+/**
+ * @file freeRam.h
+ * @author Douglas Quigg (dstroy0 dquigg123@gmail.com)
+ * @brief cross platform freeRam()
+ * @version 1.0
+ * @date 2022-05-18
+ *
+ * @copyright Copyright (c) 2022
+ */
+/*
+ Copyright (C) 2022 Douglas Quigg (dstroy0) <dquigg123@gmail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 3 as published by the Free Software Foundation.
+ */
+
+#if !defined(__CROSS_PLATFORM_FREERAM_H__)
+    #define __CROSS_PLATFORM_FREERAM_H__
 
     #include <Arduino.h>
 
     // not esp32 or esp8266 freeRam()
+    // https://forum.arduino.cc/t/how-much-static-ram-is-used/84286/8
     #if !defined(ESP32) || !defined(ESP8266)
         int freeRam()
         {
@@ -13,7 +31,7 @@
         }
     #endif
 
-    // esp "freeRam" built-in ESP.getFreeHeap
+    // esp "freeRam" built-in ESP.getFreeHeap()
     #if defined(ESP32) || defined(ESP8266)
         #define freeRam() ESP.getFreeHeap()
     #endif
