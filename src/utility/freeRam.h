7277ae8cb3d2b464a91d1a56288a859c111ec9ac
Douglas Quigg <dquigg123@gmail.com>
2022-05-18 20:05:39 -0400

    Update freeRam.h

--- src/utility/freeRam.h
+++ src/utility/freeRam.h
@@ -41,7 +41,7 @@
         extern unsigned long _heap_start;
         extern unsigned long _heap_end;
         extern char* __brkval;
-        int freeram()
+        int freeRam()
         {
           return (char*)&_heap_end - __brkval;
         }
