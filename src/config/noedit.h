267f30f9a65be7a2d2217be1e81ece9bffc944da
Douglas Quigg <dquigg123@gmail.com>
2022-05-28 11:19:45 -0400

    Update noedit.h

--- src/config/noedit.h
+++ src/config/noedit.h
@@ -1,7 +1,7 @@
 /**
    @file noedit.h
    @author Douglas Quigg (dstroy0 dquigg123@gmail.com)
-   @brief InputHandler library C includes
+   @brief InputHandler library C includes, do not edit
    @version 1.0
    @date 2022-05-16
 
@@ -19,62 +19,66 @@
     #define __INPUTHANDLER_NOEDIT_H__
 
     #include <Arduino.h>
-    #include "config/config.h"
-    #include "config/advanced_config.h"
+    #include "config/config.h" // user config file
+    #include "config/advanced_config.h" // advanced and debugging toggleable options
 
     /*
-        do not edit the sections below unless you know what will happen
+        DO NOT EDIT the sections below unless you know what will happen, incorrect entry
+        can introduce undefined behavior.
     */
 
     // function-like macros
-    #define nprms(x) (sizeof(x) / sizeof((x)[0])) // gets the number of elements in an array
-    #define buffsz(x) nprms(x)                    // gets the number of elements in an array
-    #define nelems(x) nprms(x)                    // gets the number of elements in an array
-    #define S(x) #x                               // gnu direct # stringify macro
-    #define STR(x) S(x)                           // gnu indirect # stringify macro
+    #define nprms(x) (sizeof(x) / sizeof((x)[0])) ///< gets the number of elements in an array
+    #define buffsz(x) nprms(x)                    ///< gets the number of elements in an array
+    #define nelems(x) nprms(x)                    ///< gets the number of elements in an array
+    #define S(x) #x                               ///< gnu direct # stringify macro
+    #define STR(x) S(x)                           ///< gnu indirect # stringify macro
     // end function-like macros
 
     // file location directive    
-    #define LOC __FILE__:__LINE__
-    #define LOCATION STR(LOC)
+    #define LOC __FILE__ : __LINE__ ///< direct non-stringified file and line macro
+    #define LOCATION STR(LOC)       ///< indirect stringified file and line macro
     // end file location directive
 
     // portability directives
-    #if defined(ARDUINO_SAMD_VARIANT_COMPLIANCE)
-        #include <avr/dtostrf.h>
-        #include "utility/vsnprintf.h"
-        #define vsnprintf_P vsnprintf
-        #undef pgm_read_dword
+    #if defined(ARDUINO_SAMD_VARIANT_COMPLIANCE) ///< SAMD portability
+        #include <avr/dtostrf.h>       // implement dtostrf
+        #include "utility/vsnprintf.h" // implement vsnprintf
+        #define vsnprintf_P vsnprintf  // this platform does not use vsnprintf_P
+        #undef pgm_read_dword          // use a different macro for pgm_read_dword
+        ///< PROGMEM fix macro
         #define pgm_read_dword(addr) ({     \
             typeof(addr) _addr = (addr);    \
             *(const unsigned long*)(_addr); \
         })
     #endif
 
-    #if defined(__MBED_CONFIG_DATA__)
-        #include <avr/dtostrf.h>
-        #include "utility/vsnprintf.h"
-        #define vsnprintf_P vsnprintf
-        #undef pgm_read_dword
+    #if defined(__MBED_CONFIG_DATA__) ///< MBED portability
+        #include <avr/dtostrf.h>       // implement dtostrf
+        #include "utility/vsnprintf.h" // implement vsnprintf
+        #define vsnprintf_P vsnprintf  // this platform does not use vsnprintf_P
+        #undef pgm_read_dword          // use a different macro for pgm_read_dword
+        ///< PROGMEM fix macro
         #define pgm_read_dword(addr) ({     \
             typeof(addr) _addr = (addr);    \
             *(const unsigned long*)(_addr); \
         })
     #endif
 
-    #if defined(ARDUINO_SAM_DUE)
-        #include <avr/dtostrf.h>
-        #include "utility/vsnprintf.h"
-        #define vsnprintf_P vsnprintf
-        #undef pgm_read_dword
+    #if defined(ARDUINO_SAM_DUE) ///< DUE portability
+        #include <avr/dtostrf.h>       // implement dtostrf
+        #include "utility/vsnprintf.h" // implement vsnprintf
+        #define vsnprintf_P vsnprintf  // this platform does not use vsnprintf_P
+        #undef pgm_read_dword          // use a different macro for pgm_read_dword
+        ///< PROGMEM fix macro
         #define pgm_read_dword(addr) ({     \
             typeof(addr) _addr = (addr);    \
             *(const unsigned long*)(_addr); \
         })
     #endif
 
-    #if defined(TEENSYDUINO)
-        // pgm/ram section type conflict fix
+    #if defined(TEENSYDUINO) ///< teensy portability
+        // pgm/ram section type conflict fix macros (fixes PROGMEM addressing)
         #define QUO(x) #x
         #define QLINE(x, y) \
             QUO(x)          \
@@ -93,7 +97,8 @@
     */
 
     // UI_ALL_WCC_CMD UserInput::_calcCmdMemcmpRanges and UserInput::_compareCommandToString specific (magic number!)
-    #define UI_ALL_WCC_CMD ((IH::memcmp_ranges_type)-1) // UI_ALL_WCC_CMD is Type MAX
+    #define UI_ALL_WCC_CMD ((IH::memcmp_ranges_type)-1) ///< UI_ALL_WCC_CMD is Type MAX
+
 /**
  * @namespace IH
  * @brief "auto" type namespace
@@ -140,7 +145,7 @@ typedef uint8_t memcmp_ranges_type;     // if your commands are longer than 255,
     #endif
     #if UI_MAX_COMMANDS > UINT32_MAX
         #pragma message(" at " LOCATION)
-        #warning UI_MAX_ARGS cannot be greater than UINT32_MAX
+        #error UI_MAX_COMMANDS cannot be greater than UINT32_MAX
     #endif // end UI_MAX_COMMANDS
 
 
