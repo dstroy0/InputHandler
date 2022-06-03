8284a712c70144f8932964b293cb38726503d83d
Douglas Quigg <dquigg123@gmail.com>
2022-06-02 20:42:47 -0400

    docs

--- cli_gen_tool/cli_gen_tool.py
+++ cli_gen_tool/cli_gen_tool.py
@@ -1,3 +1,17 @@
+##
+# @file cli_gen_tool.py
+# @author Douglas Quigg (dstroy0 dquigg123@gmail.com)
+# @brief InputHandler CLI gen tool
+# @version 0.1
+# @date 2022-05-28
+# @copyright Copyright (c) 2022
+
+# Copyright (C) 2022 Douglas Quigg (dstroy0) <dquigg123@gmail.com>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 3 as published by the Free Software Foundation.
+
 import sys
 
 import csv   #for learning basic data entry
