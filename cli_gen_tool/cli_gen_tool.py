18ed3ff1ed565352e3ff6bac1a35b1776f1ab29e
Douglas Quigg <dquigg123@gmail.com>
2022-06-11 12:01:37 -0400

    button/action methods

--- cli_gen_tool/cli_gen_tool.py
+++ cli_gen_tool/cli_gen_tool.py
@@ -23,6 +23,63 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # connect objects to methods
+        # file menu
+        self.ui.actionOpen.triggered.connect(self.open_file)
+        self.ui.actionSave.triggered.connect(self.save_file)
+        self.ui.actionPreferences.triggered.connect(self.gui_settings)
+        # about menu
+        self.ui.actionAbout.triggered.connect(self.gui_about)
+        self.ui.actionInputHandler_Documentation.triggered.connect(self.gui_documentation)
+        # buttons
+        # tab 1
+        self.ui.editButton_1.clicked.connect(self.clicked_edit_tab_one)
+        self.ui.clearButton_1.clicked.connect(self.clicked_clear_tab_one)
+        self.ui.defaultButton_1.clicked.connect(self.clicked_default_tab_one)
+        # tab 2
+        self.ui.newButton_2.clicked.connect(self.clicked_new_tab_two)
+        self.ui.editButton_2.clicked.connect(self.clicked_edit_tab_two)
+        self.ui.deleteButton_2.clicked.connect(self.clicked_delete_tab_two)
+
+    # actions
+    # TODO
+    def open_file(self):
+        print('open file')
+    # TODO
+    def save_file(self):
+        print('save file')
+    # TODO
+    def gui_settings(self):
+        print('preferences')
+    # TODO
+    def gui_about(self):
+        print('about')
+    # TODO
+    def gui_documentation(self):
+        print('docs')
+
+    # buttons
+    # tab 1
+    # TODO
+    def clicked_edit_tab_one(self):
+        print('clicked tab 1 edit')
+    # TODO
+    def clicked_clear_tab_one(self):
+        print('clicked tab 1 clear')
+    # TODO
+    def clicked_default_tab_one(self):
+        print('clicked tab 1 default')
+    # tab 2
+    # TODO
+    def clicked_edit_tab_two(self):
+        print('clicked tab 2 edit')
+    # TODO
+    def clicked_new_tab_two(self):
+        print('clicked tab 2 new')
+    # TODO
+    def clicked_delete_tab_two(self):
+        print('clicked tab 2 delete')
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
