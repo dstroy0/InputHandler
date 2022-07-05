4a21f48633dc4800830a3ee350cb8614a90fce3c
dstroy0 <dstroy0@users.noreply.github.com>
2022-07-05 17:53:38 +0000

    deploy: 1224c0f673f2e38a9a08682fbbfe2abb9a40158b

--- d3/d6c/classcli__gen__tool_1_1_main_window.js
+++ d3/d6c/classcli__gen__tool_1_1_main_window.js
@@ -2,7 +2,7 @@ var classcli__gen__tool_1_1_main_window =
 [
     [ "__init__", "d3/d6c/classcli__gen__tool_1_1_main_window.html#adac917eabdfc9b68e04e8857097ed97a", null ],
     [ "add_data_delimiter_row", "d3/d6c/classcli__gen__tool_1_1_main_window.html#a625b43381cdb82f4e819b25a2c82e776", null ],
-    [ "add_settings_tree_table_row", "d3/d6c/classcli__gen__tool_1_1_main_window.html#a1d79eeaa1db83b7c6ae3c70c3a9d711e", null ],
+    [ "add_settings_tree_table_row", "d3/d6c/classcli__gen__tool_1_1_main_window.html#a74c50fc1053f045e40cebd9da61e64f4", null ],
     [ "add_start_stop_data_delimiter_row", "d3/d6c/classcli__gen__tool_1_1_main_window.html#ad161655bdfe639db0cd8c8333940b752", null ],
     [ "append_to_arg_csv", "d3/d6c/classcli__gen__tool_1_1_main_window.html#a9b031a6e2d63a651611f66027c375996", null ],
     [ "argument_handling_changed", "d3/d6c/classcli__gen__tool_1_1_main_window.html#a4e5b9bb40b992e80aab71c5e71a90084", null ],
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['add_5fdata_5fdelimiter_5frow_0',['add_data_delimiter_row',['../d3/d6c/classcli__gen__tool_1_1_main_window.html#a625b43381cdb82f4e819b25a2c82e776',1,'cli_gen_tool::MainWindow']]],
-  ['add_5fsettings_5ftree_5ftable_5frow_1',['add_settings_tree_table_row',['../d3/d6c/classcli__gen__tool_1_1_main_window.html#a1d79eeaa1db83b7c6ae3c70c3a9d711e',1,'cli_gen_tool::MainWindow']]],
+  ['add_5fsettings_5ftree_5ftable_5frow_1',['add_settings_tree_table_row',['../d3/d6c/classcli__gen__tool_1_1_main_window.html#a74c50fc1053f045e40cebd9da61e64f4',1,'cli_gen_tool::MainWindow']]],
   ['add_5fstart_5fstop_5fdata_5fdelimiter_5frow_2',['add_start_stop_data_delimiter_row',['../d3/d6c/classcli__gen__tool_1_1_main_window.html#ad161655bdfe639db0cd8c8333940b752',1,'cli_gen_tool::MainWindow']]],
   ['addcommand_3',['addCommand',['../dc/d4b/class_user_input.html#aa6067a65dc10a5c4546ce789a3edd4d1',1,'UserInput']]],
   ['append_5fto_5farg_5fcsv_4',['append_to_arg_csv',['../d3/d6c/classcli__gen__tool_1_1_main_window.html#a9b031a6e2d63a651611f66027c375996',1,'cli_gen_tool::MainWindow']]],
