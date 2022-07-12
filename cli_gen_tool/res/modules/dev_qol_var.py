fed2eda5ed895592d6e49cdc4aa04f0c8c6ab9dd
Douglas Quigg <dquigg123@gmail.com>
2022-07-12 19:26:07 -0400

    strings and filestrings

--- cli_gen_tool/res/modules/dev_qol_var.py
+++ cli_gen_tool/res/modules/dev_qol_var.py
@@ -16,6 +16,7 @@ from PySide6.QtCore import QDir
 
 # dev qol var
 # these are here for ease of access, clarity, or both
+
 version = 0.1  # save serialization
 ## Library pathing
 path = QDir()
@@ -202,18 +203,20 @@ code_preview_text_line_offset = 4
 
 ## setup.h
 # these should be in a list or dict
-setup_h_class_output_string = "(output_buffer, buffsz(output_buffer), input_prm)"
+setup_h_class_output_string = "({input_prm}, {outputbuffer}, buffsz({outputbuffer}))"
 setup_h_constructor_string = "UserInput {objectname}{classoutput};"
 setup_h_addcommand_string = "    {objectname}.addCommand({commandparametersname});\n"
 setup_h_options_string_list = [
     "    {objectname}.listCommands(); // formats output_buffer with the command list\n",
     "    {objectname}.outputToStream({stream}); // class output\n",
 ]
-setup_h_output_buffer_string = "\nchar output_buffer[{buffersize}] = {bufferchar}; // output buffer size\n"
+setup_h_output_buffer_string = (
+    "\nchar output_buffer[{buffersize}] = {bufferchar}; // output buffer size\n"
+)
 setup_h_filestring = """
 #if !defined(__CLI_SETUP__)
     #define __CLI_SETUP__
-    #include <InputHandler.h>
+    #include "InputHandler.h"
     #include "functions.h"
     #include "parameters.h"
 
@@ -263,17 +266,100 @@ void InputHandler_setup()
 
 ## functions.h
 # prototypes only
+function_prototype = "void {functionname}(UserInput* _{objectname});"
+functions_h_filestring = """
+#if !defined(__FUNCTIONS_H__)
+    #define __FUNCTIONS_H__
+    #include "InputHandler.h"
+    
+{functionprototypes}
 
+#endif
+// end of file
+"""
 # end functions.h
 
 ## functions.cpp
 # non-proto only
+function_string = """void {functionname}(UserInput* _{objectname})
+{{
+    {statements}
+}}
+"""
+functions_cpp_filestring = """
+#if !defined(__FUNCTIONS_CPP__)
+    #define __FUNCTIONS_CPP__
+    #include "functions.h"
+    
+{functions}
 
+#endif
+// end of file
+"""
 # end functions.cpp
 
 ## parameters.h
 # only CommandParameters and nests
+commandparameters_string = """
+/**
+   @brief CommandParameters struct for {functionname}
+*/
+const PROGMEM CommandParameters {functionname}_param[1] = {
+    {functionname},                     // this is allowed to be NULL, if this is NULL and the terminating subcommand function ptr is also NULL nothing will launch (error)
+    {wildcardflag},             // no_wildcards or has_wildcards, default WildCard Character (wcc) is '*'
+    {commandstring},                   // command string
+    {lencommandstring},                        // command string characters
+    {parentid},                     // parent id
+    {commandid},                     // this command id
+    {commanddepth},                     // command depth
+    {commandsubcommands},                        // subcommands
+    {argumenthandling}, // argument handling
+    {minnumargs},                        // minimum expected number of arguments
+    {maxnumargs},                        // maximum expected number of arguments
+    /* UITYPE arguments */
+    {argtypearray} // use NO_ARGS if the function expects no arguments    
+};
+{commandconstructor}
+"""
 
+nested_commandparameters_string = """
+/**
+   @brief CommandParameters struct for {functionname}
+*/
+const PROGMEM CommandParameters {functionname}_param[{numberofchildren}] = 
+{{
+    {{
+      {functionname},                     // this is allowed to be NULL, if this is NULL and the terminating subcommand function ptr is also NULL nothing will launch (error)
+      {wildcardflag},             // no_wildcards or has_wildcards, default WildCard Character (wcc) is '*'
+      {commandstring},                   // command string
+      {lencommandstring},                        // command string characters
+      {parentid},                     // parent id
+      {commandid},                     // this command id
+      {commanddepth},                     // command depth
+      {commandsubcommands},                        // subcommands
+      {argumenthandling}, // argument handling
+      {minnumargs},                        // minimum expected number of arguments
+      {maxnumargs},                        // maximum expected number of arguments
+      /* UITYPE arguments */
+      {argtypearray} // use NO_ARGS if the function expects no arguments    
+    }},
+    {children}    
+}};
+{commandconstructor}
+"""
+
+commandconstructor_string = "CommandConstructor {functionname}_({functionname}_param); //  help has a command string, and function specified"
+
+parameters_h_filestring = """
+#if !defined(__PARAMETERS_H__)
+    #define __PARAMETERS_H__
+    #include "setup.h"
+    
+{parameters}
+
+#endif
+// end of file
+"""
 # end parameters.h
 
 # end dev qol var
