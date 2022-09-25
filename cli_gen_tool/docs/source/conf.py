9ed71f4b98e1766f0e7b1c45d617f2b99308a1fd
Douglas Quigg <dquigg123@gmail.com>
2022-09-24 20:26:03 -0400

    basic sphinx config, groundwork theme

--- cli_gen_tool/docs/source/conf.py
+++ cli_gen_tool/docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'Douglas Quigg (dstroy0@gmail.com)'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,5 +23,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'groundwork'
 html_static_path = ['_static']
