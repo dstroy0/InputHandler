f00d9fec8107a5c63057ca28437b6b2f5628d986
Douglas Quigg <dquigg123@gmail.com>
2022-09-28 14:14:05 -0400

    Update conf.py

--- cli_gen_tool/docs/source/conf.py
+++ cli_gen_tool/docs/source/conf.py
@@ -48,4 +48,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
+html_static_path = ["./cli_gen_tool/docs/source/_static"]
