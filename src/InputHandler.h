b926b8a41ca4e6b34cf91afd71112907a8846d1b
Douglas Quigg <dquigg123@gmail.com>
2022-10-03 17:49:40 -0400

    remove doxygen groups

--- src/InputHandler.h
+++ src/InputHandler.h
@@ -22,15 +22,7 @@
     #include "config/noedit.h"
 
 /**
- * @defgroup typedefs
- *
- * @brief InputHandler typedefs
- *
- * @{
- */
-
-/**
- * @brief IH_pname is a char array typedef the size of UI_PROCESS_NAME_PGM_LEN
+ * @brief a char array typedef the size of UI_PROCESS_NAME_PGM_LEN
  *
  * This is a char array, you can change the macro in src/config/config.h
  */
@@ -38,30 +30,24 @@ typedef char IH_pname[UI_PROCESS_NAME_PGM_LEN];
 
 /**
  * @brief IH_eol is a char array typedef the size of UI_EOL_SEQ_PGM_LEN
+ * 
  * This is a char array, you can change the macro in src/config/config.h
  */
 typedef char IH_eol[UI_EOL_SEQ_PGM_LEN];
 
 /**
  * @brief IH_input_cc is a char array typedef the size of UI_INPUT_CONTROL_CHAR_SEQ_PGM_LEN
+ * 
  * This is a char array, you can change the macro in src/config/config.h
  */
 typedef char IH_input_cc[UI_INPUT_CONTROL_CHAR_SEQ_PGM_LEN];
 
 /**
  * @brief IH_wcc is a two char array that represents the wildcard char and a null terminator
+ * 
  * This is a char array, you can change the macro in src/config/config.h
  */
 typedef char IH_wcc[2];
-/**@} */ // end defgroup typedefs
-
-/**
- * @defgroup ENUMS
- *
- * @brief InputHandler ENUMS
- *
- * @{
- */
 
 /**
  * @brief command identifier enum
@@ -138,15 +124,6 @@ enum class UITYPE
     NO_ARGS,    /*<  no arguments expected */
     _LAST       /*<  reserved */
 };
-/**@} */ // end defgroup ENUMS
-
-/**
- * @defgroup structs
- *
- * @brief InputHandler structs
- *
- * @{
- */
 
 /**
  * @brief InputProcessDelimiterSequences struct holds user defined input data delimiters
@@ -239,23 +216,15 @@ struct CommandParameters
     IH::ui_max_args_t max_num_args;               /*< maximum number of arguments this command expects 0 - UI_MAX_ARGS, cannot be less than num_args */
     UITYPE arg_type_arr[UI_MAX_ARGS_PER_COMMAND]; /*< argument UITYPE array */
 };
-/**@} */ // end defgroup structs
-
-/**
- * @defgroup constants
- *
- * @brief InputHandler constants
- *
- * @{
- */
 
 /**
  * @brief library constants located in PROGMEM
  *
  *
  * This namespace's purpose is to avoid name collision
- * and to consolidate the library's PROGMEM v
+ * and to consolidate the library's PROGMEM variables
  *
+ * @namespace ihconst
  */
 namespace ihconst
 {
@@ -333,15 +302,6 @@ const InputProcessParameters PROGMEM default_parameters = {
     &ihconst::start_stop_sequences         /*< default process default start/stop sequences */
 };
 } // end namespace ihconst
-/**@} */ // end defgroup constants
-
-/**
- * @defgroup classes
- *
- * @brief InputHandler classes
- *
- * @{
- */
 
 /**
  * @class CommandConstructor
@@ -863,10 +823,6 @@ private:
     UI_COMPARE _compareCommandToString(CommandConstructor* cmd, size_t prm_idx, char* str);
     // end private methods
 };
-
-/**@}
- *
- */ // end defgroup classes
 
 #endif // header guard include
 
