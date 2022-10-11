c8ec7702ec4de12a4afe33fea3933a19a24d15e1
Douglas Quigg <dquigg123@gmail.com>
2022-10-11 14:20:07 -0400

    Update noedit.h

--- src/config/noedit.h
+++ src/config/noedit.h
@@ -146,7 +146,7 @@ typedef bool input_type_match_flags_type; ///< UserInput private member type (fu
 /**
  * @brief sized off of `#UI_MAX_COMMANDS_IN_TREE`.
  *
- * config.h.UI_MAX_COMMANDS_IN_TREE is user set, the _MAX variables in the
+ * @link UI_MAX_COMMANDS_IN_TREE @endlink is user set, the _MAX variables in the
  * following code snippet are set elsewhere.
  *
  * @code{.c}
