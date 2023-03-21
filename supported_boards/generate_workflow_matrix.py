adde88100a06d3a524ec0b2b09a13116c0455949
Douglas Quigg <dquigg123@gmail.com>
2023-03-21 10:24:16 -0400

    format, workflow testing

--- supported_boards/generate_workflow_matrix.py
+++ supported_boards/generate_workflow_matrix.py
@@ -1,16 +1,22 @@
+##
+# @file generate_workflow_matrix.py
+# @author Douglas Quigg (dstroy0 dquigg123@gmail.com)
+# @brief generates json to use as build workflow matrix
+# @version 1.0
+# @date 2023-03-20
+# @copyright Copyright (c) 2022
+# Copyright (C) 2022 Douglas Quigg (dstroy0) <dquigg123@gmail.com>
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 3 as published by the Free Software Foundation.
+
 import os
 import sys
 import argparse
 
-exclude = "#"
+exclude = "#" # lines starting with # are ignored
 platforms = {
-    "arduino": [
-        "adafruit", 
-        "esp32", 
-        "esp8266", 
-        "teensyduino", 
-        "arduino", 
-        "rpi"],
+    "arduino": ["adafruit", "esp32", "esp8266", "teensyduino", "arduino", "rpi"],
     "platformio": [
         "atmelavr",
         "atmelmegaavr",
@@ -46,8 +52,8 @@ def get_boards_for_platform(file_text: list, platform: str) -> list:
     if file_text == [] or platform == "":
         return f"get boards error!\nfile_text:\n{file_text}\n\nplatform: {platform}\n"
     for line_num in range(len(file_text)):
-        line_text = file_text[line_num].lower().strip()        
-        if ("boards" in line_text or "fqbn" in line_text) and platform in line_text:                        
+        line_text = file_text[line_num].lower().strip()
+        if ("boards" in line_text or "fqbn" in line_text) and platform in line_text:
             line_num += 1
             while file_text[line_num] != "\n":
                 ret_list.append(file_text[line_num].strip())
@@ -56,23 +62,24 @@ def get_boards_for_platform(file_text: list, platform: str) -> list:
                     break
                 if file_text[line_num] == "\n":
                     break
-            break        
+            break
     if not bool(ret_list):
         return f"platform not found or no boards are supported for this platform: {platform}"
     return ret_list
+
 
 def generate_workflow_matrix(board_list: list) -> str:
     if not isinstance(board_list, list):
         print(board_list)
         sys.exit(1)
-    matrix_text = "{\"boards\":["
+    matrix_text = '{"boards":['
     for item in board_list:
         ignore = False
         for ex in exclude:
             if ex in item:
                 ignore = True
         if not ignore:
-            line = '"' + item.strip() 
+            line = '"' + item.strip()
             if item != board_list[-1]:
                 line += '", '
             else:
@@ -93,7 +100,7 @@ def cli() -> list:
         nargs=1,
         type=str,
         required=True,
-        help='-c <compiler type>',
+        help="-c <compiler type>",
         metavar="",
     )
     parser.add_argument(
@@ -118,7 +125,9 @@ def cli() -> list:
         if args.platform[0].lower().strip() in platforms[ret_list[0]]:
             ret_list.append(str(args.platform[0]).lower().strip())
         else:
-            print(f"compiler: {ret_list[0]}\nplatform unknown: {args.platform[0].lower().strip()}")
+            print(
+                f"compiler: {ret_list[0]}\nplatform unknown: {args.platform[0].lower().strip()}"
+            )
     if len(ret_list) < 2:  # fatal error
         parser.print_help()
         sys.exit(1)
